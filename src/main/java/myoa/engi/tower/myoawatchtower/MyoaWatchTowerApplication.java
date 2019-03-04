7ef54f30bcb78c1baa863f7da9d55f1480e3876d
DESKTOP-FSAUHQP\myoat <myoatm@naver.com>
2019-03-05 00:18:05 +0900

    Initialize base project

---
+++ src/main/java/myoa/engi/tower/myoawatchtower/MyoaWatchTowerApplication.java
@@ -0,0 +1,13 @@
+package myoa.engi.tower.myoawatchtower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyoaWatchTowerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyoaWatchTowerApplication.class, args);
+    }
+
+}
